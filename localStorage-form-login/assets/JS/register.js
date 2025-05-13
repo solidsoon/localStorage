d96024a057e90d7160f7dd3b1bf49cf51b2cefb7
Kauan Souza <162659253+solidsoon@users.noreply.github.com>
2025-05-12 21:05:21 -0300

    Add files via upload

---
+++ localStorage-form-login/assets/JS/register.js
@@ -0,0 +1,8 @@
+const logUser = document.getElementById("log-usuario")
+const password = document.getElementById("log-password")
+const confirmPassword = document.getElementById("log-confirmar-senha")
+const btnRegister = document.getElementById("SignInBtn")
+
+btnRegister.addEventListener("click", () =>  {
+      alert("CANDANGO")
+})
